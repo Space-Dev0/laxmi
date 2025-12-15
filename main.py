b7f58ae8b88105ca6dc68a851537130754cea224
Space-Dev0 <179112172+Space-Dev0@users.noreply.github.com>
2025-12-15 22:26:53 +0530

    fix: Spelling error Asia to Asia

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ def wait_for_start_time(start_time_str):
     try:
         now = datetime.now(tz=ZoneInfo('Asia/Kolkata'))
         t = datetime.strptime(start_time_str, "%H:%M").time()
-        target = datetime.combine(now.date(), t,tzinfo=ZoneInfo('Aisa/Kolkata'))
+        target = datetime.combine(now.date(), t,tzinfo=ZoneInfo('Asia/Kolkata'))
 
         if now < target:
             sleep_seconds = (target - now).total_seconds()
