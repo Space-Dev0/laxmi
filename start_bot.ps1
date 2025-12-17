cf1f39a9610fb844fab58daa8a6de4024fc071da
Space-Dev0 <179112172+Space-Dev0@users.noreply.github.com>
2025-12-17 17:41:08 +0530

    fix: use tmux instead of screen in pwsh script

--- start_bot.ps1
+++ start_bot.ps1
@@ -1,1 +1,1 @@
-ssh -t -i "laxmi.pem" ubuntu@ec2-52-205-10-59.compute-1.amazonaws.com "screen -S laxmi_bot bash -l -c 'cd laxmi/; micro config.yaml; uv run main.py; exec bash'"
+ssh -t -i "laxmi.pem" ubuntu@ec2-52-205-10-59.compute-1.amazonaws.com "grep -q 'set -g mouse on' ~/.tmux.conf || echo -e 'set -g mouse on\nset -s set-clipboard on' >> ~/.tmux.conf; tmux new-session -A -s laxmi_bot ""bash -l -c 'cd laxmi/; micro -clipboard terminal config.yaml; uv run main.py; exec bash'"""
